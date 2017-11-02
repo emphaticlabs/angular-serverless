624b7c067bd11d49abae412da7fce793421ea250
Angular CLI <angular-cli@angular.io>
2017-11-02 15:02:25 -0600

    chore: initial commit from @angular/cli

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'liga-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'liga';
+}
